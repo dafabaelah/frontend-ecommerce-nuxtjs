e240b1cefc3a1f6be88af819d5dec1688770de93
dafabaelah <dafabaelah12@gmail.com>
2024-06-25 17:02:48 +0700

    setup nextjs

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,15 +9,22 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/images/logo.png' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'},
+    ],
+    script: [
+      { src: '/js/coreui.bundle.min.js' }, 
+      { src: 'https://app.sandbox.midtrans.com/snap/snap.js', 'data-client-key': 'paste_client_Key_midtrans_disini' }, 
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/style.min.css',
+    '@/assets/css/custom.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -35,9 +42,71 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  axios: {
+    baseURL: 'http://localhost:8000'
+  },
+
+  loading: {
+    color: 'white',	// <-- color
+    height: '5px'	// <-- height
+  },
+
+  // Target Deployment
+  target: 'server',
+
+  //rendering mode SSR	
+  ssr: true,
+
+  auth: {
+    strategies: {
+
+      // strategy untuk admin
+      admin: {
+        scheme: 'local',
+        token: {
+          property: 'token',
+          required: true,
+          type: 'Bearer'
+        },
+        user: {
+          property: 'user',
+          // autoFetch: true
+        },
+        endpoints: {
+          login: { url: '/api/admin/login', method: 'post', propertyName: 'token' },
+          logout: { url: '/api/admin/logout', method: 'post'},
+          user: { url: '/api/admin/user', method: 'get', propertyName: 'user' }
+        },
+      },
+
+      // strategy untuk customer
+      customer: {
+        scheme: 'local',
+        token: {
+          property: 'token',
+          required: true,
+          type: 'Bearer'
+        },
+        user: {
+          property: 'user',
+          // autoFetch: true
+        },
+        endpoints: {
+          login: { url: '/api/customer/login', method: 'post', propertyName: 'token' },
+          logout: { url: '/api/customer/logout', method: 'post'},
+          user: { url: '/api/customer/user', method: 'get', propertyName: 'user' }
+        },
+      },
+
+    },
+  },
+
 }
