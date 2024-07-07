2103899df1683ffa104985a1a68133ee7ae61c32
dafabaelah <dafabaelah12@gmail.com>
2024-07-07 10:49:55 +0700

    membuat CI github action & deploy on docker pada heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,9 @@
 export default {
+  server: {
+    port: process.env.PORT || 3000, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'frontend-ecommerce',
