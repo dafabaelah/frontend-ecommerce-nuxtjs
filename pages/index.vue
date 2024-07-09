a3c5ddcb65553045367e91655c22be6280a11e50
dafabaelah <dafabaelah12@gmail.com>
2024-07-09 09:13:55 +0700

    menambahkan view dashboard customer, view invoice customer

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <Tutorial/>
+  <div class="mt-custom mb-5">
+    <div class="fade-in">
+      <h1 class="text-center">SANTRI KODING</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  
 }
 </script>
